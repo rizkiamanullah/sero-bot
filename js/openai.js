2c9bc56163a25bb25091d7838f0c49d8a05d5029
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2022-12-19 21:56:29 +0700

    update initbot

--- js/openai.js
+++ js/openai.js
@@ -44,7 +44,6 @@ function fetchBot(prompt) {
             data: JSON.stringify(data),
             success: function(res){
                 var anwser = res.choices[0].text; 
-                console.log(anwser);
                 var anwser_clean = anwser.replace(usedPrompt,''); 
                 var anwser_clean = anwser_clean.replace('AI Assistant: ',''); 
                 var anwser_clean = anwser_clean.replace('AI: ',''); 
@@ -56,10 +55,60 @@ function fetchBot(prompt) {
                 }
                 chat_log.push(data);
                 $('#chat_box').append(blob_bot(anwser_clean));
-                $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+                $("html, body").animate({ scrollTop: $(document).height() }, 0);
             },
             error: function(e){
                 console.log(e);
+            }
+        });
+    });
+}
+
+function initBot() {
+    const url = 'https://api.openai.com/v1/completions';
+    var usedPrompt = getMessage();
+    var not = '';
+    database
+    .ref('open_key')
+    .on('value', function(s){
+        var k = s.val();
+        not = k[0].key;
+
+        const data = {
+            "model": "text-davinci-003",
+            "prompt": usedPrompt,
+            "temperature": 0.9,
+            "max_tokens": 150,
+            "top_p": 1,
+            "frequency_penalty": 0.0,
+            "presence_penalty": 0.6,
+            "stop": [" Human:", " AI:"],
+            "echo": true,
+        };
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            contentType: 'application/json',
+            beforeSend: function(xhr){
+                xhr.setRequestHeader('Authorization', `Bearer ${not}`);
+            },
+            data: JSON.stringify(data),
+            success: function(res){
+                var anwser = res.choices[0].text; 
+                var anwser_clean = anwser.replace(usedPrompt,''); 
+                var anwser_clean = anwser_clean.replace('AI Assistant: ',''); 
+                var anwser_clean = anwser_clean.replace('AI: ',''); 
+                var data = {
+                    message: 'AI: '+anwser_clean,
+                    type: 'Bot',
+                }
+                chat_log.push(data);
+                console.log('bot initiated');
+                console.log(anwser_clean);
+            },
+            error: function(e){
+                console.log('init failed: '+e);
             }
         });
     });
